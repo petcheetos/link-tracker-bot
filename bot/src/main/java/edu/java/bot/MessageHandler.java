a22cbe6e083e03c9bfba78f1b7809d7adf9106ba
Anastasia <whydema@gmail.com>
2024-02-17 11:49:07 +0300

    checkstyle fix

--- bot/src/main/java/edu/java/bot/MessageHandler.java
+++ bot/src/main/java/edu/java/bot/MessageHandler.java
@@ -4,8 +4,8 @@ import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
-import java.util.List;
 import edu.java.bot.service.CommandService;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
