1bf755ca06b0f47658562440355790caca407b3a
Anastasia <whydema@gmail.com>
2024-02-17 11:47:29 +0300

    refactor

--- bot/src/main/java/edu/java/bot/MessageHandler.java
+++ bot/src/main/java/edu/java/bot/MessageHandler.java
@@ -1,10 +1,11 @@
-package edu.java.bot.service;
+package edu.java.bot;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
 import com.pengrad.telegrambot.request.SendMessage;
 import java.util.List;
+import edu.java.bot.service.CommandService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
