d9e8504ea9e58bd2576b7db901f5a81f623bb9a4
Anastasia <whydema@gmail.com>
2024-03-05 20:54:44 +0300

    client config

--- scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
+++ scrapper/src/main/java/edu/java/configuration/ClientConfiguration.java
@@ -1,20 +1,32 @@
 package edu.java.configuration;
 
+import edu.java.clients.BotClient;
 import edu.java.clients.GitHubClient;
 import edu.java.clients.StackoverflowClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class ClientConfiguration {
+    private final WebClient.Builder builder;
+
+    public ClientConfiguration(WebClient.Builder builder) {
+        this.builder = builder;
+    }
 
     @Bean
     public GitHubClient gitHubClient(ApplicationConfig applicationConfig) {
-        return new GitHubClient(applicationConfig.githubBaseUrl().baseUrl());
+        return new GitHubClient(builder, applicationConfig.githubBaseUrl().baseUrl());
     }
 
     @Bean
     public StackoverflowClient stackoverflowClient(ApplicationConfig applicationConfig) {
-        return new StackoverflowClient(applicationConfig.stackoverflowBaseUrl().baseUrl());
+        return new StackoverflowClient(builder, applicationConfig.stackoverflowBaseUrl().baseUrl());
+    }
+
+    @Bean
+    public BotClient botClient(ApplicationConfig applicationConfig) {
+        return new BotClient(builder, applicationConfig.botBaseUrl().baseUrl());
     }
 }
