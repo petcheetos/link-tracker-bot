ac5d397ad5d99d9007c05d4a88f13dd64e3bc392
Anastasia <whydema@gmail.com>
2024-03-14 14:32:17 +0300

    small changes

--- migrations/create_chat_link_table.sql
+++ migrations/create_chat_link_table.sql
@@ -1,7 +1,7 @@
 --liquibase formatted sql
 
 create table if not exists chat_links (
-	id_chat bigint not null references chat (id) on delete cascade,
-	id_link bigint not null references link (id) on delete cascade,
-	primary key (id_chat, id_link)
+    chat_id bigint not null references chat (id) on delete cascade,
+    link_id bigint not null references link (id) on delete cascade,
+    primary key (chat_id, link_id)
 );
--- migrations/create_chat_table.sql
+++ migrations/create_chat_table.sql
@@ -2,6 +2,5 @@
 
 create table if not exists chat(
 	id bigint not null,
-	created_at timestamp with time zone not null,
 	primary key(id)
 );
