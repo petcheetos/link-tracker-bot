ce0bbbf5d97552ce522c8a80a533969010b422d6
Anastasia <whydema@gmail.com>
2024-04-02 22:02:02 +0300

    remove checks

--- scrapper/src/main/java/edu/java/services/jdbc/JdbcChatService.java
+++ scrapper/src/main/java/edu/java/services/jdbc/JdbcChatService.java
@@ -1,31 +1,19 @@
 package edu.java.services.jdbc;
 
-import edu.java.exception.RequestException;
 import edu.java.repository.ChatRepository;
 import edu.java.services.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service
-@RequiredArgsConstructor
-public class JdbcChatService implements ChatService {
+@Service @RequiredArgsConstructor public class JdbcChatService implements ChatService {
 
     private final ChatRepository chatRepository;
 
-    @Override
-    public void registerChat(long chatId) {
-        if (chatRepository.exist(chatId)) {
-            throw new RequestException("Chat already registered");
-        }
+    @Override public void registerChat(long chatId) {
         chatRepository.add(chatId);
     }
 
-    @Override
-    public void deleteChat(long chatId) {
-        if (!chatRepository.exist(chatId)) {
-            throw new RequestException("Chat does not exist");
-        }
+    @Override public void deleteChat(long chatId) {
         chatRepository.remove(chatId);
     }
 }
-
