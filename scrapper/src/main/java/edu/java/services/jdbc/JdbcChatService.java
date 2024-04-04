535aea7e421866a138ab339275133da7f3caf16f
Anastasia <whydema@gmail.com>
2024-04-04 12:24:17 +0300

    refactor

--- scrapper/src/main/java/edu/java/services/jdbc/JdbcChatService.java
+++ scrapper/src/main/java/edu/java/services/jdbc/JdbcChatService.java
@@ -5,15 +5,18 @@ import edu.java.services.ChatService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
-@Service @RequiredArgsConstructor public class JdbcChatService implements ChatService {
+@Service
+@RequiredArgsConstructor public class JdbcChatService implements ChatService {
 
     private final ChatRepository chatRepository;
 
-    @Override public void registerChat(long chatId) {
+    @Override
+    public void registerChat(long chatId) {
         chatRepository.add(chatId);
     }
 
-    @Override public void deleteChat(long chatId) {
+    @Override
+    public void deleteChat(long chatId) {
         chatRepository.remove(chatId);
     }
 }
